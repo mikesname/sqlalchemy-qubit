be9420ca2f35dad2eed659a242cea31e74570809
Mike Bryant <mikesname@gmail.com>
2011-09-19 11:59:17 +0100

    Add utility functions for creating a test database and loading SQL.

--- sqlaqubit/__init__.py
+++ sqlaqubit/__init__.py
@@ -2,17 +2,35 @@
 SQLAlchemy classes for Qubit database.
 """
 
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker
 import models
 
 test_engine = create_engine("mysql+mysqldb://qubit:changeme@localhost/test_ehriqubit?charset=utf8&use_unicode=0")
 
 
+def load_test_db(dbpath, **kwargs):
+    """Load an SQLite file for testing."""
+    engine = create_engine("sqlite:///%s" % dbpath, **kwargs)
+    init_models(engine)
+    return models.Session(bind=engine)
+
+
+def load_test_sql(sqlpath, **kwargs):
+    """Load an SQLite db for testing."""
+    engine = create_engine("sqlite://", **kwargs)
+    with open(sqlpath, "r") as f:
+        cur = engine.raw_connection()
+        cur.executescript(f.read())
+    init_models(engine)
+    return models.Session(bind=engine)
+
+
 def init_models(engine):
     """Decorator for generating an I18N table
     for the given source table, and placing
     an accessor relationship on the source."""
+    models.unregister_i18n()
     models.Base.metadata.bind = engine
     models.Base.metadata.reflect()
     i18nupdate = {}
